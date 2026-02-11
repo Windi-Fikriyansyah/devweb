babace5aa26700627c60cfecdaee57a65fddd94b
windi fikriyansyah <diwin321@gmail.com>
2026-02-11 15:47:05 +0700

    baru432

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,9 +84,18 @@ export default function RootLayout({
         <WhatsAppFloating />
         <Footer />
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
-          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
         />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `}
+        </Script>
       </body>
     </html>
   );
